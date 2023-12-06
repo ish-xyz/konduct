488c6a7f3907971509cde633e7d9faa7aa715d86
ish-xyz <ishamaraia@gmail.com>
2023-12-06 17:33:15 +0000

    fixed cobra parsing

--- cmd/root.go
+++ cmd/root.go
@@ -24,9 +24,8 @@ var (
 	controllerMode bool
 	interval       int64
 	//exporterType   string
-	kubeconfig   string
-	sourceDir    string
-	templatesDir string
+	kubeconfig string
+	testsDir   string
 
 	rootCmd = cobra.Command{
 		Long: "A controller and CLI to run e2e tests on Kubernetes",
@@ -34,66 +33,66 @@ var (
 	}
 )
 
-func init() {
-
-	rootCmd.Flags().StringP("kube-config", "k", "~/.kube/config", "path to the kubeconfig file, if empty uses in-cluster method")
-	rootCmd.Flags().StringP("source-dir", "s", "", "Filesystem path to test cases, if empty will load tests cases from the Kubernetes API")
-	//rootCmd.Flags().StringP("labels", "l", "", "Run tests associated with specific labels")
-	//rootCmd.Flags().StringP("exporter", "e", "stdout", "Define exporter: stdout, prometheus, pushgateway, json-file, text-file")
-	rootCmd.Flags().Int64P("interval", "i", 30, "In controller mode, this settings defines the interval between one test run and the other")
-	rootCmd.Flags().BoolP("debug", "d", false, "Run program in debug mode")
-	rootCmd.Flags().BoolP("controller", "c", false, "Run program in controller mode")
-
-	kubeconfig, err = rootCmd.Flags().GetString("kube-config")
-
-	checkError(err)
-
-	sourceDir, err = rootCmd.Flags().GetString("source-dir")
-	checkError(err)
-
-	// exporterType, err = rootCmd.Flags().GetString("exporter")
-	// checkError(err)
-
-	interval, err = rootCmd.Flags().GetInt64("interval")
-	checkError(err)
-
-	debug, err = rootCmd.Flags().GetBool("debug")
-	checkError(err)
-
-	controllerMode, err = rootCmd.Flags().GetBool("controller")
-	checkError(err)
-
-}
-func expand(path string) string {
-	if strings.HasPrefix(path, "~/") {
-		usr, _ := user.Current()
-		dir := usr.HomeDir
-		// Use strings.HasPrefix so we don't match paths like
-		// "/something/~/something/"
-		path = filepath.Join(dir, path[2:])
-	}
-
-	return path
-}
-
-func checkError(err error) {
-	if err != nil {
-		logrus.Fatalln(err)
-	}
-}
-
 func Execute() {
 	rootCmd.Execute()
 }
 
+func init() {
+
+	rootCmd.Flags().StringP("kube-config", "k", "~/.kube/config", "path to the kubeconfig file, if empty uses in-cluster method")
+	rootCmd.Flags().StringP("dir", "d", "", "Filesystem path to test cases, if empty will load test cases from the Kubernetes API")
+	//rootCmd.Flags().StringP("labels", "l", "", "Run tests associated with specific labels")
+	//rootCmd.Flags().StringP("exporter", "e", "stdout", "Define exporter: stdout, prometheus, pushgateway, json-file, text-file")
+	rootCmd.Flags().Int64P("interval", "i", 30, "In controller mode, this settings defines the interval between one test run and the other")
+	rootCmd.Flags().Bool("debug", false, "Run program in debug mode")
+	rootCmd.Flags().BoolP("controller", "c", false, "Run program in controller mode")
+
+}
+
+func parseFlags(cmd *cobra.Command) error {
+
+	kubeconfig, err = cmd.Flags().GetString("kube-config")
+	if err != nil {
+		return err
+	}
+
+	testsDir, err = cmd.Flags().GetString("dir")
+	if err != nil {
+		return err
+	}
+
+	// exporterType, err = rootCmd.Flags().GetString("exporter")
+	// checkError(err)
+
+	interval, err = cmd.Flags().GetInt64("interval")
+	if err != nil {
+		return err
+	}
+
+	debug, err = cmd.Flags().GetBool("debug")
+	if err != nil {
+		return err
+	}
+
+	controllerMode, err = cmd.Flags().GetBool("controller")
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func run(cmd *cobra.Command, args []string) {
 
-	var err error
+	err := parseFlags(cmd)
+	checkError(err)
 
 	// Run in debug mode
 	if debug {
 		logrus.SetLevel(logrus.DebugLevel)
 	}
+
+	fmt.Println(kubeconfig, testsDir)
 
 	kubeconfig = expand(kubeconfig)
 
@@ -108,9 +107,9 @@ func run(cmd *cobra.Command, args []string) {
 
 	// Init Tests Loader
 	var ldr loader.Loader
-	if sourceDir != "" {
+	if testsDir != "" {
 		// load from filesystem
-		ldr, err = loader.NewFSLoader(sourceDir, templatesDir)
+		ldr, err = loader.NewFSLoader(testsDir, "")
 	} else {
 		// load from kube api
 		logrus.Fatalln("CRDs are not implemented yet")
@@ -161,4 +160,22 @@ func getClients(restConfig *rest.Config) (*kubernetes.Clientset, *dynamic.Dynami
 	}
 
 	return cs, dc, nil
+}
+
+func expand(path string) string {
+	if strings.HasPrefix(path, "~/") {
+		usr, _ := user.Current()
+		dir := usr.HomeDir
+		// Use strings.HasPrefix so we don't match paths like
+		// "/something/~/something/"
+		path = filepath.Join(dir, path[2:])
+	}
+
+	return path
+}
+
+func checkError(err error) {
+	if err != nil {
+		logrus.Fatalln(err)
+	}
 }
