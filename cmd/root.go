b52215c0a2f876966fe61ecdcfec08bc71188da6
ish-xyz <ishamaraia@gmail.com>
2023-12-08 00:26:32 +0000

    rewriting CLI to pass config file only

--- cmd/root.go
+++ cmd/root.go
@@ -1,14 +1,11 @@
 package cmd
 
 import (
+	"fmt"
 	"os/user"
 	"path/filepath"
 	"strings"
 
-	"github.com/ish-xyz/kubetest/pkg/client"
-	"github.com/ish-xyz/kubetest/pkg/controller"
-	"github.com/ish-xyz/kubetest/pkg/exporter"
-	"github.com/ish-xyz/kubetest/pkg/loader"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"k8s.io/client-go/dynamic"
@@ -22,15 +19,7 @@ const (
 )
 
 var (
-	err            error
-	debug          bool
-	controllerMode bool
-	interval       int64
-	kubeconfig     string
-	testsDir       string
-
-	stdout      bool
-	pushgateway string
+	err error
 
 	rootCmd = cobra.Command{
 		Long: "A controller and CLI to run e2e tests on Kubernetes",
@@ -42,78 +31,29 @@ func Execute() {
 	rootCmd.Execute()
 }
 
+// config:
+
+/*
+  kubeconfig: ~/.kube/config
+  interval: 4h
+  debug: true
+  source:
+    cluster: false
+	filesystem:
+	  directory: ./path
+  report:
+	prometheus:
+	  address: 0.0.0.0:9090
+	pushgateway:
+	  gateway: 0.0.0.0:9091
+*/
+
 func init() {
-	// TODO: add label selectors
-
-	// Marked flags
-	rootCmd.Flags().StringP("kube-config", "k", "", "path to the kubeconfig file, if empty uses in-cluster method")
-	rootCmd.Flags().StringP("dir", "d", "", "Filesystem path to test cases, if empty will load test cases from the Kubernetes API")
-	rootCmd.Flags().Int64P("interval", "i", -1, "In controller mode, this settings defines the interval between one test run and the other")
-	rootCmd.Flags().BoolP("controller", "c", false, "Run program in controller mode")
-	rootCmd.Flags().StringP("pushgateway", "g", "", "Define push gateway address")
-	rootCmd.Flags().Bool("stdout", false, "Print report to stdout")
-
-	// Optional flags
-	rootCmd.Flags().Bool("debug", false, "Run program in debug mode")
-
-	// MARKERS:
-
-	// can't define --kube-config and --dir together \
-	// AND you have to define at least one of them
-	rootCmd.MarkFlagsMutuallyExclusive("pushgateway", "stdout")
-	rootCmd.MarkFlagsOneRequired("pushgateway", "stdout")
-
-	// can't define --kube-config and --dir together \
-	// AND you have to define at least one of them
-	rootCmd.MarkFlagsMutuallyExclusive("kube-config", "dir")
-	rootCmd.MarkFlagsOneRequired("dir", "kube-config")
-
-	// if --controller is specific --interval should be too
-	rootCmd.MarkFlagsRequiredTogether("controller", "interval")
-
+	rootCmd.Flags().StringP("config", "c", "", "pass kubetest config file path")
+	rootCmd.MarkFlagRequired("config")
 }
 
-func parseFlags(cmd *cobra.Command) error {
-
-	kubeconfig, err = cmd.Flags().GetString("kube-config")
-	if err != nil {
-		return err
-	}
-
-	testsDir, err = cmd.Flags().GetString("dir")
-	if err != nil {
-		return err
-	}
-
-	interval, err = cmd.Flags().GetInt64("interval")
-	if err != nil {
-		return err
-	}
-
-	debug, err = cmd.Flags().GetBool("debug")
-	if err != nil {
-		return err
-	}
-
-	controllerMode, err = cmd.Flags().GetBool("controller")
-	if err != nil {
-		return err
-	}
-
-	pushgateway, err = cmd.Flags().GetString("pushgateway")
-	if err != nil {
-		return err
-	}
-
-	stdout, err = cmd.Flags().GetBool("stdout")
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func getRestConfig(kubeconfig string) (*rest.Config, error) {
+func GetRestConfig(kubeconfig string) (*rest.Config, error) {
 	var restConfig *rest.Config
 	// Load kubeconfig
 	if kubeconfig == "" {
@@ -124,7 +64,7 @@ func getRestConfig(kubeconfig string) (*rest.Config, error) {
 	return restConfig, err
 }
 
-func getClients(restConfig *rest.Config) (*kubernetes.Clientset, *dynamic.DynamicClient, error) {
+func GetClients(restConfig *rest.Config) (*kubernetes.Clientset, *dynamic.DynamicClient, error) {
 	// Allocate clients
 	dc, errx := dynamic.NewForConfig(restConfig)
 
@@ -141,7 +81,7 @@ func getClients(restConfig *rest.Config) (*kubernetes.Clientset, *dynamic.Dynami
 	return cs, dc, nil
 }
 
-func expand(path string) string {
+func Expand(path string) string {
 	if strings.HasPrefix(path, "~/") {
 		usr, _ := user.Current()
 		dir := usr.HomeDir
@@ -161,56 +101,59 @@ func checkError(err error) {
 
 func run(cmd *cobra.Command, args []string) {
 
-	err := parseFlags(cmd)
+	configFile, err := cmd.Flags().GetString("config")
 	checkError(err)
 
-	// Run in debug mode
-	if debug {
-		logrus.SetLevel(logrus.DebugLevel)
-	}
-
-	restConfig, err := getRestConfig(expand(kubeconfig))
-	checkError(err)
-
-	// Init Kube Client
-	clientset, dynclient, err := getClients(restConfig)
-	checkError(err)
-
-	kubeclient := client.NewKubeClient(clientset, dynclient, restConfig)
-
-	// Init Tests Loader
-	var ldr loader.Loader
-	if testsDir != "" {
-		// load from filesystem
-		ldr, err = loader.NewFSLoader(testsDir, "")
-	} else {
-		// load from kube api
-		ldr, err = loader.NewKubeLoader(kubeclient)
-	}
-	checkError(err)
-
-	// Init Exporter
-	exp := getExporter(debug)
-
-	// Init Controller
-	ctrl := controller.NewController(ldr, kubeclient, exp, interval)
-
-	// Execute
-	if controllerMode {
-		logrus.Fatalln("controller mode is not implemented yet")
-	} else {
-		err = ctrl.Run(debug)
-	}
-
-	checkError(err)
+	fmt.Println(configFile)
 }
 
-func getExporter(verbose bool) exporter.Exporter {
-	if stdout {
-		return exporter.NewStdoutExporter(verbose)
-	}
+// 	// Run in debug mode
+// 	if debug {
+// 		logrus.SetLevel(logrus.DebugLevel)
+// 	}
 
-	// TODO: validate address
-	return exporter.NewPrometheusExporter("pushgateway", pushgateway)
+// 	restConfig, err := getRestConfig(expand(kubeconfig))
+// 	checkError(err)
 
-}
+// 	// Init Kube Client
+// 	clientset, dynclient, err := getClients(restConfig)
+// 	checkError(err)
+
+// 	kubeclient := client.NewKubeClient(clientset, dynclient, restConfig)
+
+// 	// Init Tests Loader
+// 	var ldr loader.Loader
+// 	if testsDir != "" {
+// 		// load from filesystem
+// 		ldr, err = loader.NewFSLoader(testsDir, "")
+// 	} else {
+// 		// load from kube api
+// 		ldr, err = loader.NewKubeLoader(kubeclient)
+// 	}
+// 	checkError(err)
+
+// 	// Init Exporter
+// 	exp := getExporter(debug)
+
+// 	// Init Controller
+// 	ctrl := controller.NewController(ldr, kubeclient, exp, interval)
+
+// 	// Execute
+// 	if controllerMode {
+// 		logrus.Fatalln("controller mode is not implemented yet")
+// 	} else {
+// 		err = ctrl.Run(debug)
+// 	}
+
+// 	checkError(err)
+// }
+
+// func getExporter(verbose bool) exporter.Exporter {
+// 	if stdout {
+// 		return exporter.NewStdoutExporter(verbose)
+// 	}
+
+// 	// TODO: validate address
+// 	return exporter.NewPrometheusExporter("pushgateway", pushgateway)
+
+// }
